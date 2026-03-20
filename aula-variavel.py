0480bf31a423ab13d2c9ffc15d37eea9649e0e18
Eduardomors <moreira.eduardodudu@gmail.com>
2026-03-19 22:28:48 -0300

    aula variavel

---
+++ aula-variavel.py
@@ -0,0 +1,26 @@
+print("ola mundo")
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # comentario de strings
+
+'''
+comentarios de múltiplas linhas
+'''
+
+#variáveis
+nome = "Eduardo" # str
+idade = 17 # int
+peso = 60.4 # float
+
+print(nome,idade,peso)
+print(f"OIIIi, {nome} !!!")
+
+#inputs = simulação de forms no cmd
+nome = input("Digite  seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade = 1
+
+print(nome, idade)
+print(nova_idade)
+
